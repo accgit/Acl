cdc626480ebe899cf60d4080db8860602b90d92d
Zdeněk Papučík <zdenek.papucik@gmail.com>
2018-01-18 12:02:08 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -12,7 +12,7 @@ CREATE TABLE `privileges` (
 
 -- ---- insert data in privileges table:
 INSERT INTO `privileges` (`privilegeId`, `name`) VALUES
-(NULL, ':all'),
+(NULL, '.*'),
 (NULL, 'default');
 
 -- ---- create resources table:
