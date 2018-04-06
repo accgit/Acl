3bb67eb4b6927d5fc292879c5d0429ce3a64bdab
Zdeněk Papučík <zdenek.papucik@gmail.com>
2018-04-06 07:29:11 +0200

    Update Resources.php

--- src/acl/repository/Resources.php
+++ src/acl/repository/Resources.php
@@ -73,12 +73,12 @@ class Resources extends BaseRepository
 	public function save(Acl\Entity\Resources $entity)
 	{
 		if (!$entity->getId()) {
-			$row = $this->db->insert($this->table, Database\Iterator::set($entity))->execute();
+			$row = $this->db->insert($this->table, Database\Iterator::toArray($entity))->execute();
 			$this->caches->removeCache(Acl\Authorizator::ACL_CACHE);
 			return $row;
 		} else {
 			$row = $this->db
-				->update($this->table, Database\Iterator::set($entity))
+				->update($this->table, Database\Iterator::toArray($entity))
 				->where('resourceId = ?', $entity->getId())
 				->execute();
 
